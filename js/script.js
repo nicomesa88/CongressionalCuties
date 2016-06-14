1c1f8e2442e29d7158d8b8b6b20aa950a6ba41e7
Nicolas Mesa <nicolasmesa@Nicolass-MacBook-Air.local>
2016-06-14 08:51:34 -0500

    finished homework, starting adventure mode.

--- js/script.js
+++ js/script.js
@@ -3,28 +3,30 @@ console.log($)
 var congressionalPromise = $.getJSON('https://congress.api.sunlightfoundation.com/legislators?apikey=f69289663395468db48b632d79c7f151')
 
 
-var legsCards = document.querySelector('.pols')
+var legsCards = document.querySelector('#container')
 
 
 var handleData = function(apiResponse) {
     console.log(apiResponse)
     var legArray = apiResponse.results
-    var htmlString = '<p>'+'</p>'
+    var htmlString = ''
     for (var i = 0; i < legArray.length; i ++) {
         var legislatorObject = legArray[i]
         console.log(legislatorObject.state_name)
-        htmlString += '<h2 class= "name">' + legislatorObject.first_name + ' ' + legislatorObject.last_name + '</h2>'
-        htmlString += '<h3 class= "partyState"> ' + legislatorObject.title + ' -- ' + legislatorObject.party + ' - ' + legislatorObject.state_name + '</h3>'
+        htmlString += '<div>'
+        htmlString += '<h1 class= "name">' + legislatorObject.first_name + ' ' + legislatorObject.last_name + '</h1>'
+        htmlString += '<h2 class= "partyState"> ' + legislatorObject.title + ' -- ' + legislatorObject.party + ' - ' + legislatorObject.state_name + '</h2>'
         htmlString += '<ul>'
                 htmlString += '<li class = "email"> email: ' + legislatorObject.email +'</li>'
                 htmlString += '<li class = "website"> website: ' +legislatorObject.website +'</li>'
                 htmlString += '<li class = "facebook"> facebook: ' +legislatorObject.facebook_id +'</li>'
                 htmlString += '<li class = "twitter"> twitter: ' +legislatorObject.twitter_id +'</li>'
         htmlString += '</ul>'
-        htmlString += '<h4 class = "termEnd"> Term End ' + legislatorObject.term_end +'</h4>'
+        htmlString += '<h3 class = "termEnd"> Term End ' + legislatorObject.term_end +'</h3>'
+        htmlString += '</div>'
     }
 
-    legsCards.innerHTML = htmlString
+    legsCards.innerHTML += htmlString
 
 }
 
